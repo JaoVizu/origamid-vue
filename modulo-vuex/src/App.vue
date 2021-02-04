01cbf29bda1ef30c23973d858c46741f641c94eb
JaoVizu <joaovizu2015@gmail.com>
2021-02-04 19:57:24 -0300

    modulo do Vuex

---
+++ modulo-vuex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Bem Vindo - {{$store.state.user}}</h1>
+    <User />
+    <Curso />
+  </div>
+</template>
+
+<script>
+import User from './components/User.vue'
+import Curso from './components/Curso.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Curso, User
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ modulo-vuex/src/components/Aula.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Aula {{aulasCompletas}} completas, esse é o usuário: {{user}}</p>
+    <button @click="completarAula">Completar Aula</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    ...mapMutations(['completarAula']),
+  },
+  computed: {
+    ...mapState(['user', 'aulasCompletas'])
+  }
+}
+</script>
+
+<style>
+
+</style>
